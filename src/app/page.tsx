3ad1a51eddf2ed42e6546df788c182f22792ebe0
Christian Areas <me@areas.me>
2026-03-14 19:03:02 -0700

    Wired Plaid-related frontend to backend.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@
 // Dependencies.
 // --------------------------------------------------------------------------------
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { usePlaidLink } from "react-plaid-link"
 import { CreatePlaidLinkTokenResponse } from "@/lib/schemas/api"
 
@@ -16,25 +16,36 @@ export default function Home() {
 	// Initial state.
 	const [plaidLinkToken, setPlaidLinkToken] = useState<string | null>(null)
 
+	// Fetch the Plaid link token.
+	useEffect(() => {
+		async function fetchPlaidLinkToken() {
+			const response = await fetch("/api/plaid/create-link-token", {
+				method: "POST",
+			})
+			setPlaidLinkToken(
+				CreatePlaidLinkTokenResponse.parse(await response.json())
+					.plaidLinkToken,
+			)
+		}
+		fetchPlaidLinkToken()
+	}, [])
+
 	// Open Plaid Link.
 	const { open, ready } = usePlaidLink({
 		token: plaidLinkToken,
-		onSuccess: (publicToken, metadata) => {
-			console.log("public token:", publicToken)
-			console.log("metadata:", metadata)
+		onSuccess: (plaidPublicToken) => {
+			fetch("/api/plaid/exchange-public-token", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ plaidPublicToken }),
+			}).catch(console.error)
+		},
+		onExit: (error) => {
+			if (error?.error_code === "INVALID_LINK_TOKEN") {
+				setPlaidLinkToken(null)
+			}
 		},
 	})
-
-	// Fetch a Plaid link token.
-	async function fetchPlaidLinkToken() {
-		const response = await fetch("/api/plaid/create-link-token", {
-			method: "POST",
-		})
-
-		setPlaidLinkToken(
-			CreatePlaidLinkTokenResponse.parse(await response.json()).plaidLinkToken,
-		)
-	}
 
 	// Return the home page.
 	return (
@@ -44,8 +55,8 @@ export default function Home() {
 		>
 			<button
 				type="button"
-				onClick={() => (plaidLinkToken ? open() : fetchPlaidLinkToken())}
-				disabled={!!plaidLinkToken && !ready}
+				onClick={() => open()}
+				disabled={!ready}
 				className="flex items-center gap-2 rounded-lg border border-neutral-500 px-4 py-2 text-neutral-700 transition hover:bg-neutral-50 disabled:opacity-50 dark:border-neutral-300 dark:bg-neutral-800 dark:text-neutral-50 dark:hover:bg-neutral-950"
 			>
 				Open Plaid Link
